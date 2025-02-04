7e3815205581cbf95a14f33a9e55c6b5f58b5dd3
shahriarnaiem-rafi <shahriarnaiemrafi33.com>
2025-02-04 10:29:22 +0600

    up alfa

--- Projjj/Project For Final/Alfa/src/main.jsx
+++ Projjj/Project For Final/Alfa/src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Router from "./Router/Router.jsx";
-import { LoginProvider } from "./context/LoginContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <LoginProvider>
+      
       <RouterProvider router={Router} />
-      </LoginProvider>
+      
   </StrictMode>
 );
